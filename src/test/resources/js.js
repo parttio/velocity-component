2f1523ed7bca16b8e5b02a1549dda74878f6b2dc
Matti Tahvonen <matti@vaadin.com>
2023-08-07 16:46:29 +0300

    Added missing example file

---
+++ src/test/resources/js.js
@@ -0,0 +1,8 @@
+// Similar example as inlined, but this is a separate file
+// This approach is useful when you have larger
+// javascript code that you want to keep in a separate file
+setTimeout(() => {${serverElement}.style.color = "red";}, 4000);
+
+var p = document.createElement("p");
+p.textContent = "Hello, I'm coming through js.js file in resources. specialparameter1: $specialparameter1";
+this.appendChild(p);
